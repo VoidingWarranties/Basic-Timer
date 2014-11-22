e6ba745ff712490faadc0e1a2b54cf798a0a1f9d
Jordan Horwich <jordan@voidingwarranties.com>
2014-11-21 22:24:28 -0600

    Make millisecondsBetweenTicks return signed values
    Fixes #1

--- BasicTimer.h
+++ BasicTimer.h
@@ -22,13 +22,11 @@ class BasicTimer {
     return milliseconds;
   }
 
-  // Returns the number of milliseconds between two timers' ticks. start MUST
-  // have been ticked before end otherwise the value returned will not be
-  // correct as it is an unsigned value. Perhaps this could be changed in the
-  // future.
-  friend uint64_t millisecondsBetweenTicks(const BasicTimer& start,
+  // The value returned will be positive when end is ticked after start, and
+  // negative when start is ticked after end.
+  friend int64_t millisecondsBetweenTicks(const BasicTimer& start,
                                            const BasicTimer& end) {
-    uint64_t milliseconds = (end.tick_.time - start.tick_.time) * 1000;
+    int64_t milliseconds = (end.tick_.time - start.tick_.time) * 1000;
     milliseconds += end.tick_.millitm - start.tick_.millitm;
     return milliseconds;
   }
